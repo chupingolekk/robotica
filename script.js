0232b376200da883c91c140d04b51e9660053425
chupingolekk <eduardo.biniara@escola.pr.gov.br>
2026-08-23 21:51:29 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+script.js
+// =====================================================
+// PROJETO DE ROBÓTICA
+// JavaScript
+// =====================================================
+
+console.log("RobotLab carregado com sucesso! 🤖");
+
+
+// Pequeno efeito no carregamento da página
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    document.body.classList.add("loaded");
+
+});
